60db361ec6887268092ab826c9bd1c2c1c162193
claudialiguori <claudia.liguori@student.h-farm.com>
2019-11-23 06:56:53 -0500

    added documentation

--- cc_validation/luhn.py
+++ cc_validation/luhn.py
@@ -1,11 +1,28 @@
-iin_ranges = [
+"""
+This file contains CC validation functions
+"""
+
+
+def check_issuer(card_number):
+
+    "Function to check CC issuer"
+
+    """
+    List containing CC issuers 
+
+    Col.1: start of range 
+    Col.2: end of range 
+    Col.3: service issuer 
+
+    """
+
+    iin_ranges = [
         [340000,379999, "American Express"],
         [510000,559999, "Mastercard"],
         [560000,599999, "Maestro"],
         [400000,499999, "Visa"]
         ]
-
-def check_issuer(card_number):
+    
     iin = int(card_number[:6])
     for start, end, issuer in iin_ranges:
         if iin >= start and iin<=end:
@@ -14,10 +31,16 @@ def check_issuer(card_number):
 
 
 def digits_of(n):
+
+    "Function to create a list of integers from string"
+
     return [int(d) for d in str(n)]
 
 
 def LuhnChecksum(card_number ):
+
+    "Function to compute the checksum"
+
     digits=digits_of(card_number)
     odd_digits=digits[-1::-2]
     even_digits=digits[-2::-2]
@@ -29,6 +52,14 @@ def LuhnChecksum(card_number ):
 
 
 def isLuhnValid(card_number):
+
+    "Function to print the CC validation"
+
+    """
+    Args:
+         card_number: string
+    """
+
     if LuhnChecksum(card_number) == 0:
         print("The number that was given as input:{} is a valid CC number".format(card_number))
     else:
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #! /usr/bin/env python3
+
+# IMPORT MODULES
+
 from cc_validation.luhn import isLuhnValid
+
+
+# DEFINE VARIABLES
 
 valid = "4556194895263730"
 invalid = "8556194895263730" 
+
+
+# BODY
 
 isLuhnValid(valid)
 isLuhnValid(invalid)
