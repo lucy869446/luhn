a5124788bee99f2b9b145e7e01a056e823a3ee4c
claudialiguori <claudia.liguori@student.h-farm.com>
2019-11-23 09:32:04 -0500

    fixed the code style

--- cc_validation/luhn.py
+++ cc_validation/luhn.py
@@ -8,24 +8,24 @@ def check_issuer(card_number):
     "Function to check CC issuer"
 
     """
-    List containing CC issuers 
+    List containing CC issuers
 
-    Col.1: start of range 
-    Col.2: end of range 
-    Col.3: service issuer 
+    Col.1: start of range
+    Col.2: end of range
+    Col.3: service issuer
 
     """
 
     iin_ranges = [
-        [340000,379999, "American Express"],
-        [510000,559999, "Mastercard"],
-        [560000,599999, "Maestro"],
-        [400000,499999, "Visa"]
+        [340000, 379999, "American Express"],
+        [510000, 559999, "Mastercard"],
+        [560000, 599999, "Maestro"],
+        [400000, 499999, "Visa"]
         ]
-    
+
     iin = int(card_number[:6])
     for start, end, issuer in iin_ranges:
-        if iin >= start and iin<=end:
+        if iin >= start and iin <= end:
             return issuer
     return "unknown"
 
@@ -37,13 +37,13 @@ def digits_of(n):
     return [int(d) for d in str(n)]
 
 
-def LuhnChecksum(card_number ):
+def LuhnChecksum(card_number):
 
     "Function to compute the checksum"
 
-    digits=digits_of(card_number)
-    odd_digits=digits[-1::-2]
-    even_digits=digits[-2::-2]
+    digits = digits_of(card_number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
     Checksum = 0
     Checksum += sum(odd_digits)
     for l in even_digits:
@@ -61,7 +61,11 @@ def isLuhnValid(card_number):
     """
 
     if LuhnChecksum(card_number) == 0:
-        print("The number that was given as input:{} is a valid CC number".format(card_number))
+        print((
+             "The number that was given as input: {} is a valid CC number")
+             .format(card_number))
     else:
-        print("The number that was given as input:{} is not a valid CC number".format(card_number))
+        print((
+             "The number that was given as input: {} is not a valid CC number")
+             .format(card_number))
     print("The issuer is: {}".format(check_issuer(card_number)))
