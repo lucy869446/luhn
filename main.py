7359dd2a9d59d62038d53f0a40bb4e8dba1449b3
lussy <lucia.zecchinato@student.h-farm.com>
2019-11-30 04:28:41 -0500

    5_Switch to argparse-started

--- main.py
+++ main.py
@@ -3,12 +3,17 @@
 # IMPORT MODULES
 
 import sys
+import argparse
 from cc_validation.luhn import isLuhnValid
 
 
 # DEFINE VARIABLES
 
-card_number = sys.argv[1]
+#card_number = sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument("cn",type=str,help="card number to be validated")
+args = parser.parse_args()
+card_number = args.cn
 
 
 # BODY
