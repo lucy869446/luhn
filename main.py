7ef78685910600805198dd6afcd066666d5f3888
Klaudia <klaudia.kollcaku@student.h-farm.com>
2019-12-14 05:39:21 -0500

    7_create databases with SQL

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 
 # IMPORT MODULES
 
-import sys
-import argparse
+import sys, argparse, subprocess
 from cc_validation.luhn import isLuhnValid
 
 
@@ -28,15 +27,39 @@ def arg_parsing():
             raise argparse.ArgumentTypeError('Boolean value expected.')
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("cn", type=str, help="card number to be validated")
+    parser.add_argument("mode", type=str, help='type "add" to add a user to db, or "validate" to validate a cn.')
+    parser.add_argument("-a", type=str, help="username to be added to db")
+    parser.add_argument("-p", type=str, help="password to be added to db")
+    parser.add_argument("-u", type=str, help="username to access the cn validation")
+    parser.add_argument("-cn", type=str, help="card number to be validated")
     parser.add_argument("-v", type=str2bool, help="verbosity", default=False)
     args = parser.parse_args()
+    mode = args.mode
+    username_to_be_added = args.a
+    username_for_login = args.u
+    password = args.p
     card_number = args.cn
     verbosity = args.v
-    return card_number, verbosity
+    return mode, username_for_login, username_to_be_added, password, card_number, verbosity
+
+def call_dbmanager(username_for_login, username_to_be_added, password):
+    if mode=='add' and username_to_be_added!=None and password!=None:
+        exit_code=subprocess.check_output(["python /home/seed/luhn/scripts/dbmanager.py -a {} -p {}".format(username_to_be_added,password)], shell=True)
+    elif mode=='validate' and username_for_login!=None and password!=None:
+         exit_code=subprocess.check_output(["python /home/seed/luhn/scripts/dbmanager.py -u {} -p {}".format(username_for_login,password)], shell=True)
+    else:
+        exit_code=1
+    return exit_code
 
 
 # BODY
 
-card_number, verbosity = arg_parsing()
-isLuhnValid(card_number, default_datafile, verbosity)
+mode, username_for_login, username_to_be_added, password, card_number, verbosity = arg_parsing()
+exit_code = call_dbmanager(username_for_login, username_to_be_added, password)
+if exit_code!='':
+    print('Error during database operation.')
+else:
+    if mode=='validate':
+        isLuhnValid(card_number, default_datafile, verbosity)
+    elif mode=='add':
+        print('New user added.')
---
+++ scripts/dbmanager.py
@@ -0,0 +1,111 @@
+# FUNCTIONS
+#! /usr/bin/env python3
+
+# IMPORT MODULES
+
+import argparse
+import sqlite3
+from sqlite3 import Error
+import sys
+import hashlib, random 
+
+
+# PARAMETERS
+
+path = r"/home/seed/luhn_db" # Important: use absolute path to db.
+
+
+# FUNCTIONS
+
+def arg_parsing():
+    "Function to parse arguments of main.py"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", type=str, help="username to be added to db")
+    parser.add_argument("-p", type=str, help="password to be added to db")
+    parser.add_argument("-u", type=str, help="username to access the cn validation")
+    args = parser.parse_args()
+    username_to_be_added = args.a
+    username_for_login = args.u
+    password = args.p
+    return username_for_login, username_to_be_added, password
+
+
+def create_connection(db_file):
+    "Create a database connection to a SQLite database."
+    conn = None
+    conn = sqlite3.connect(db_file)
+    curs = conn.cursor()
+    return conn, curs
+
+def user_db_connection(path):
+    "Connect to user db and do operations."
+    db_file = path + "/user.db"
+    conn, curs = create_connection(db_file)
+    try:
+        curs.execute('SELECT count(username) FROM user')
+    except Error:
+        curs.execute('CREATE TABLE user (username CHAR(256), salt CHAR(256));')
+        conn.commit()
+        print('User table created.')
+    return conn, curs
+
+def password_db_connection(path):
+    "Connect to password db and do operations."
+    db_file = path + "/password.db"
+    conn, curs = create_connection(db_file)
+    try:
+        curs.execute('SELECT count(username) FROM password')
+    except Error:
+        curs.execute('CREATE TABLE password (username CHAR(256), password CHAR(256));')
+        conn.commit()
+        print('Password table created.')
+    return conn, curs
+
+def hash_password(password,salt):
+    "Hash the password."
+    digest = str(salt) + password
+    for i in range(1000):
+        digest = hashlib.sha256(digest.encode('utf-8')).hexdigest()
+    return digest
+
+
+# BODY
+
+
+exit_code = ''
+
+username_for_login, username_to_be_added, password = arg_parsing()
+
+if (username_for_login==None and username_to_be_added==None) or (password==None):
+    print('Missing arguments.')
+else:
+    user_conn, user_curs = user_db_connection(path)
+    pwd_conn, pwd_curs = password_db_connection(path)
+    if username_for_login!=None:
+        user_curs.execute('SELECT salt FROM user WHERE (username="{}");'.format(username_for_login))
+        salt = user_curs.fetchall()[0][0]
+        if len(salt)>0:
+            salt = float(salt)
+            hashed_pwd = hash_password(password,salt)
+            pwd_curs.execute('SELECT count(*) FROM password WHERE (username="{}" AND password="{}");'.format(username_for_login,hashed_pwd))
+            if int(pwd_curs.fetchall()[0][0])>0:
+                exit_code = 0
+    else:
+        user_curs.execute('SELECT count(*) FROM user WHERE (username="{}");'.format(username_to_be_added))
+        user_exists = user_curs.fetchall()[0][0]
+        if user_exists==0:
+            salt = str(random.random())
+            user_curs.execute('INSERT INTO user (username,salt) VALUES("{}", "{}");'.format(username_to_be_added,salt))
+            user_conn.commit()
+            hashed_pwd = hash_password(password,salt)
+            pwd_curs.execute('INSERT INTO password (username,password) VALUES("{}", "{}");'.format(username_to_be_added,hashed_pwd))
+            pwd_conn.commit()
+            exit_code = 0
+        else:
+            print('User already exists.')
+            exit_code = 1
+
+user_conn.close()
+pwd_conn.close()
+sys.exit(exit_code)
